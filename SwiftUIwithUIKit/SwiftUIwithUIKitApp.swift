7f2bbca7d16706a4d677e4bdbdf094bdf76a29f0
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-04-06 21:55:17 +0300

    Initial Commit

---
+++ SwiftUIwithUIKit/SwiftUIwithUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIwithUIKitApp.swift
+//  SwiftUIwithUIKit
+//
+//  Created by ALEKSEY SUSLOV on 06.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIwithUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
